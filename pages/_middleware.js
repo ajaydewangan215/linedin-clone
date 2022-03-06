a1a75c6c5467e26cedc1e41707d2f448f01ee610
Ajay Dewangan <ajaydewangan215@gmail.com>
2022-03-06 16:52:59 +0530

    Redirect url

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -8,6 +8,6 @@ export async function middleware(req) {
       secret: process.env.SECRET,
       secureCookie:process.env.NODE_ENV === 'production',
     });
-    if(!session) return NextResponse.redirect("http://localhost:3000/home")
+    if(!session) return NextResponse.redirect(NEXTAUTH_URL + "/home")
   }
 }
