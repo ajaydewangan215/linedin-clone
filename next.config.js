fb8e5bfbbf7b8bf6f7840dadefc0a2205e27fb81
Ajay Dewangan <ajaydewangan215@gmail.com>
2022-03-06 16:26:11 +0530

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  darkMode:"class",
+  images: {
+    domains: ['content.linkedin.com', 'static-exp1.licdn.com', 'www.iconsdb.com', 'images.fastcompany.net']
+  },
 }
 
 module.exports = nextConfig
